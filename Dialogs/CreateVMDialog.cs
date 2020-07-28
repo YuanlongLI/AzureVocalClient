6c9aabf05b436309e279ca3e97e316b081d707d5
Feiyue Yu <fey@microsoft.com>
2020-07-28 20:27:27 +0800

    CreateVMDialog

---
+++ Dialogs/CreateVMDialog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.BotBuilderSamples.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot.Dialogs
+{
+    public class CreateVMDialog : CancelAndHelpDialog
+    {
+        public CreateVMDialog() : base(nameof(CreateVMDialog))
+        {
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+
+            }));
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+    }
+}
